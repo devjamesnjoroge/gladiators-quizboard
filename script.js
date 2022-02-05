a383d099fa43f488701f3712e908df2ce9990291
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-02-05 22:52:30 +0300

    it will toggle results div onclicking submit

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-(function () {
-  var yourName = prompt("enter your name");
-  while (yourName == null || yourName == "") {
-    var yourName = prompt("Please enter your name");
-  }
-  document
-    .querySelector("#nameAppend")
-    .append(
-      "Welcome " +
-        yourName +
-        ", each question carries 2marks. Fill in all the questions."
-    );
-})();
+// (() => {
+//   var yourName = prompt("enter your name");
+//   while (yourName == null || yourName == "") {
+//     var yourName = prompt("Please enter your name");
+//     break;
+//   }
+//   document
+//     .querySelector("#nameAppend")
+//     .append(
+//       "Welcome " +
+//         yourName +
+//         ", each question carries 2marks. Fill in all the questions."
+//     );
+// })();
+
+var hideFooter = document.querySelector(".upper-footer");
+var hideBody = document.querySelector(".classToHide");
+var hiddenClass = document.querySelector(".revealScore");
+var submitAnswers = document.querySelector("#submitt");
+submitAnswers.addEventListener("click", function () {
+  hiddenClass.classList.toggle("active");
+  hideBody.style.display = "none";
+  hideFooter.style.display = "none";
+});
