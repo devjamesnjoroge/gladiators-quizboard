fcc1ceb3ddb8b9778a81b4eb16a8e7d19d7986ae
devjamesnjoroge <james.njoroge@student.moringaschool.com>
2022-02-06 17:43:31 +0300

    it will add a toggle effect between quiz section and answers section

--- script.js
+++ script.js
@@ -33,6 +33,7 @@ if (document.getElementById("item1d").checked) {
 var hideFooter = document.querySelector(".upper-footer");
 var hideBody = document.querySelector(".classToHide");
 var hiddenClass = document.querySelector(".revealScore");
+var revealClass = document.querySelector(".active");
 var submitAnswers = document.querySelector("#submitt");
 submitAnswers.addEventListener("click", function () {
   hiddenClass.classList.toggle("active");
@@ -79,5 +80,28 @@ submitAnswers.addEventListener("click", function () {
 
   var totalPercentage = (totalScore / 12) * 100;
 
-  alert(totalPercentage);
+  var roundedAnswer = Math.round(totalPercentage);
+
+  switch (roundedAnswer) {
+    case 100:
+    case 83:
+      document
+        .querySelector("#success")
+        .append(
+          "Congratulations you scored " +
+            roundedAnswer +
+            "%. Excellently performed."
+        );
+      break;
+    case 67:
+    case 50:
+      document
+        .querySelector("#success")
+        .append("You scored " + roundedAnswer + "%. Fairly performed.");
+      break;
+    default:
+      document
+        .querySelector("#success")
+        .append("You scored " + roundedAnswer + "%. You should redo the quiz.");
+  }
 });
